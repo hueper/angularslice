5c7c6afe3d0ded8dd0ac1ce3675424d31d7bc4bd
Marcell <kissmarcell88@gmail.com>
2016-05-24 15:32:55 +0200

    extended plopfile

---
+++ plop-templates/model.ts
@@ -0,0 +1,5 @@
+export class {{properCase name}} {
+  constructor() {
+
+  }
+}
--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,6 @@
 module.exports = function (plop) {
   // create your generators here
-  plop.setGenerator('basics', {
+  plop.setGenerator('Component', {
     description: 'Generate ng2 component',
     prompts: [
       {
@@ -29,6 +29,42 @@ module.exports = function (plop) {
         type: 'add',
         path: 'src/app/{{path}}{{name}}.scss',
         templateFile: 'plop-templates/component.scss',
+      }
+    ]
+  });
+
+  plop.setGenerator('Model', {
+    description: 'Generate ng2 model',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your model?',
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/app/models/{{name}}.ts',
+        templateFile: 'plop-templates/model.ts',
+      }
+    ]
+  });
+
+  plop.setGenerator('Service', {
+    description: 'Generate ng2 service',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your service?',
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/app/services/{{name}}.ts',
+        templateFile: 'plop-templates/service.ts',
       }
     ]
   });
