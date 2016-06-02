a45a103cc645731f76947c5e4184c3ae7ecb9b4a
carathorys <balazs.gallay.budacode@gmail.com>
2016-06-02 15:04:04 +0200

    Githubpages fix

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { provide } from '@angular/core';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { APP_BASE_HREF } from '@angular/common';
-import { ROUTER_PROVIDERS } from '@angular/router';
-
- import { AppComponent } from './app/app.component.ts';
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {provide} from "@angular/core";
+import {HTTP_PROVIDERS} from "@angular/http";
+import {APP_BASE_HREF} from "@angular/common";
+import {ROUTER_PROVIDERS} from "@angular/router";
+import {AppComponent} from "./app/app.component.ts";
 
 require('./app/shared/scss/styles.scss');
 
 bootstrap(AppComponent, [
   HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
-  provide(APP_BASE_HREF, {useValue: '/'})
+  provide(APP_BASE_HREF, {useValue: location.pathname})
 ]).catch((error) => {
   console.error('Error during app bootstrapping', error)
 });
