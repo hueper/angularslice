28190516fee6716677d982cc057cf7b5f8611e0f
Tothmarton Akos <akostm@gmail.com>
2016-06-13 14:17:45 +0200

    sidebar folder counter

--- src/app/sidebar/component-element/component-element.component.ts
+++ src/app/sidebar/component-element/component-element.component.ts
@@ -1,17 +1,31 @@
 import { Component, Input } from '@angular/core';
+import {MD_ICON_DIRECTIVES} from '@angular2-material/icon';
+import { Folder, Image } from '../../shared/models';
+import {ImageService} from "../../shared/services";
 
-import { Folder } from '../../shared/models';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'component-element',
   styles: [ require('./component-element.component.scss') ],
-  template: require('./component-element.component.jade')()
+  template: require('./component-element.component.jade')(),
+  directives: [MD_ICON_DIRECTIVES]
 })
 export class ComponentElement {
 
   @Input() folder: Folder;
 
-  constructor() {
+  private images:Image[] = [];
+  private subscriptions:Subscription[] = [];
+
+  constructor(
+    private imageService:ImageService
+  ) {
+    // Look for new images without filtering
+    this.subscriptions.push(this.imageService.filter(image => this.folder.id == image.folderId).subscribe((data:Image[]) => {
+      console.log(data);
+      this.images = data;
+    }));
   }
 
 }
