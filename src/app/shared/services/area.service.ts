2d51304e77989b544b866776bd4b432c30e3f3ae
Tothmarton Akos <akostm@gmail.com>
2016-06-16 14:37:06 +0200

    area delete on folder deletion

--- src/app/shared/services/area.service.ts
+++ src/app/shared/services/area.service.ts
@@ -1,11 +1,22 @@
 import { Injectable} from '@angular/core';
 
 import { BaseService } from './base.service';
+import { FolderService } from './folder.service';
 import { Area } from '../models';
 
 @Injectable()
 export class AreaService extends BaseService<Area> {
-  constructor() {
+  constructor(
+    private folderService:FolderService
+  ) {
     super();
+
+    this.folderService.deleteSource.subscribe(folder => {
+      this.find({ folderId: folder.id }).map( (area) => {
+        this.delete(area);
+      });
+    });
   }
+
+
 }
