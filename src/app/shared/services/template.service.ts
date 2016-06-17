48ae9e7e343ff423a1a3db7948256fd0965e9404
Tothmarton Akos <akostm@gmail.com>
2016-06-17 14:37:59 +0200

    test

--- src/app/shared/services/template.service.ts
+++ src/app/shared/services/template.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { Template } from '../models';
 
-
-
 @Injectable()
 export class TemplateService extends BaseService<Template> {
   constructor() {
