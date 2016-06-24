73b6e4258c9400c5869659967c2c23fa09cc3c61
carathorys <gallayb@gmail.com>
2016-06-24 11:42:47 +0200

    dev server testing

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -28,7 +28,7 @@ export class EditorComponent {
   currentFolder: Folder;
 
   githubAuth() {
-    const authUrl = 'https://github.com/login/oauth/authorize?client_id=e57db1300ee2cb4462bf';
+    const authUrl = '/auth/github';
     const _oauthWindow = window.open(authUrl, 'GitHub Auth', 'width=800,height=400');
     _oauthWindow.addEventListener('close', (event) => {
       console.log("Closed!==================> ");
